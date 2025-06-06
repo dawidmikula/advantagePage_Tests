d615d7a401b419a4142eccd862af1491d7477848
dawidmikula <dawidmikula420@gmail.com>
2025-06-06 21:38:28 +0200

    RegisterPage Provide All Informations

--- _pages/register.page.ts
+++ _pages/register.page.ts
@@ -1,61 +1,47 @@
 import { Locator, Page } from "@playwright/test";
-
-export class RegisterPage {
+//    ACCOUNT DETAILS    //
+export class RegisterPage_AccountDetails {
   createAccountHeader: Locator;
+
   accountDetailsHeader: Locator;
   username: Locator;
-  usernameInput: Locator;
   usernameError: Locator;
   email: Locator;
-  emailInput: Locator;
   emailError: Locator;
   password: Locator;
-  passInput: Locator;
   passError: Locator;
   passReq: Locator;
   confirmPassword: Locator;
-  confirmPassInput: Locator;
   confirmPassError: Locator;
   confirmPassReq: Locator;
 
-  // Consents and Register button
-  mailingConsentCheckbox: Locator;
-  agreeConditionsCheckbox: Locator;
-  registerButton: Locator;
-
   constructor(private page: Page) {
-    // Page header
     this.createAccountHeader = this.page.getByRole("heading", {
       name: "CREATE ACCOUNT",
     });
-    // Account Details
     const accountDetailsLocator = page.getByText("ACCOUNT DETAILS *");
     this.accountDetailsHeader = accountDetailsLocator.getByRole("heading", {
       name: "ACCOUNT DETAILS",
     });
     this.username = accountDetailsLocator.locator('[sec-name="userName"] label');
-    this.usernameInput = accountDetailsLocator.locator('input[name="usernameRegisterPage"]');
     this.usernameError = accountDetailsLocator.getByText("Username field is required");
 
     this.email = accountDetailsLocator.locator('[sec-name="userEmail"] label');
-    this.emailInput = accountDetailsLocator.locator('input[name="emailRegisterPage"]');
     this.emailError = accountDetailsLocator.getByText("Email field is required");
 
     this.password = accountDetailsLocator.locator('[sec-name="userPassword"] [a-hint="Password"] label');
-    this.passInput = accountDetailsLocator.locator('input[name="passwordRegisterPage"]');
     this.passError = accountDetailsLocator.getByText("Password field is required");
     this.passReq = accountDetailsLocator.locator('[sec-name="userPassword"] [a-hint="Password"] ul');
 
     this.confirmPassword = accountDetailsLocator.locator('[sec-name="userPassword"] [a-hint="Confirm password"] label');
-    this.confirmPassInput = accountDetailsLocator.locator('input[name="confirm_passwordRegisterPage"]');
     this.confirmPassError = accountDetailsLocator.getByText("Confirm password field is");
     this.confirmPassReq = accountDetailsLocator.locator('[sec-name="userPassword"] [a-hint="Confirm password"] ul');
-
-    // Consents and Register button
-    this.mailingConsentCheckbox = this.page.locator('input[name="allowOffersPromotion"]');
-    this.agreeConditionsCheckbox = this.page.locator('input[name="i_agree"]');
-    this.registerButton = this.page.getByRole("button", { name: "REGISTER" });
   }
+
+  usernameInput: Locator;
+  emailInput: Locator;
+  passInput: Locator;
+  confirmPassInput: Locator;
 
   async provideAccountDetailsInRegisterForm(
     username: string,
@@ -63,14 +49,99 @@ export class RegisterPage {
     password: string,
     confirmPassword: string
   ) {
+    this.usernameInput = this.page.locator('input[name="usernameRegisterPage"]');
+    this.emailInput = this.page.locator('input[name="emailRegisterPage"]');
+    this.passInput = this.page.locator('input[name="passwordRegisterPage"]');
+    this.confirmPassInput = this.page.locator('input[name="confirm_passwordRegisterPage"]');
+
     await this.usernameInput.fill(username);
     await this.emailInput.fill(email);
     await this.passInput.fill(password);
     await this.confirmPassInput.fill(confirmPassword);
   }
+}
+//    PERSONAL DETAILS    //
+export class RegisterPage_PersonalDetails {
+  constructor(private page: Page) {
+    //
+  }
 
-  async agreeConditionsAndRegister() {
-    await this.agreeConditionsCheckbox.check();
+  firstNameInput: Locator;
+  lastNameInput: Locator;
+  phoneNumberInput: Locator;
+
+  async providePersonalDetailsInRegisterForm(firstName: string, lastName: string, phoneNumber: string) {
+    this.firstNameInput = this.page.locator('input[name="first_nameRegisterPage"]');
+    this.lastNameInput = this.page.locator('input[name="last_nameRegisterPage"]');
+    this.phoneNumberInput = this.page.locator('input[name="phone_numberRegisterPage"]');
+
+    await this.firstNameInput.fill(firstName);
+    await this.lastNameInput.fill(lastName);
+    await this.phoneNumberInput.fill(phoneNumber);
+  }
+}
+
+//    ADDRESS    //
+export class RegisterPage_Address {
+  constructor(private page: Page) {
+    //
+  }
+
+  countrySelect: Locator;
+  cityInput: Locator;
+  addressInput: Locator;
+  stateInput: Locator;
+  postalCodeInput: Locator;
+
+  async provideAddressInRegisterForm(
+    country: string,
+    city: string,
+    address: string,
+    state: string,
+    postalCode: string
+  ) {
+    this.countrySelect = this.page.locator('select[name="countryListboxRegisterPage"]');
+    this.cityInput = this.page.locator('input[name="cityRegisterPage"]');
+    this.addressInput = this.page.locator('input[name="addressRegisterPage"]');
+    this.stateInput = this.page.locator('input[name="state_/_province_/_regionRegisterPage"]');
+    this.postalCodeInput = this.page.locator('input[name="postal_codeRegisterPage"]');
+
+    await this.countrySelect.selectOption({ label: country });
+    await this.cityInput.fill(city);
+    await this.addressInput.fill(address);
+    await this.stateInput.fill(state);
+    await this.postalCodeInput.fill(postalCode);
+  }
+}
+
+//    BOTTOM    //
+export class RegisterPage_BottomContent {
+  userAlreadyExistError: Locator;
+  alreadyHaveAccount: Locator;
+
+  constructor(private page: Page) {
+    this.userAlreadyExistError = this.page.getByText("User name already exists");
+    this.alreadyHaveAccount = this.page.getByRole("link", { name: "ALREADY HAVE AN ACCOUNT?" });
+  }
+  mailingConsentCheckbox: Locator;
+  agreeConditionsCheckbox: Locator;
+  registerButton: Locator;
+
+  async agreeConditionsAndRegister(mailingConsentCheckbox: boolean, agreeConditionsCheckbox: boolean) {
+    this.mailingConsentCheckbox = this.page.locator('input[name="allowOffersPromotion"]');
+    this.agreeConditionsCheckbox = this.page.locator('input[name="i_agree"]');
+    this.registerButton = this.page.getByRole("button", { name: "REGISTER" });
+
+    if (mailingConsentCheckbox) {
+      await this.mailingConsentCheckbox.check();
+    } else {
+      await this.mailingConsentCheckbox.uncheck();
+    }
+    if (agreeConditionsCheckbox) {
+      await this.agreeConditionsCheckbox.check();
+    } else {
+      await this.agreeConditionsCheckbox.uncheck();
+    }
     await this.registerButton.click();
   }
 }
--- _tests/register.spec.ts
+++ _tests/register.spec.ts
@@ -1,21 +1,67 @@
 import test from "@playwright/test";
-import { RegisterPage } from "../_pages/register.page";
+import { faker, Faker } from "@faker-js/faker";
+import {
+  RegisterPage_AccountDetails,
+  RegisterPage_Address,
+  RegisterPage_BottomContent,
+  RegisterPage_PersonalDetails,
+} from "../_pages/register.page";
+import { format } from "path";
 
 test.describe("Register", () => {
-  let registerPage: RegisterPage;
+  let registerPageAccountDetails: RegisterPage_AccountDetails;
+  let registerPagePersonalDetails: RegisterPage_PersonalDetails;
+  let registerPageAddress: RegisterPage_Address;
+  let registerPageBottomContent: RegisterPage_BottomContent;
 
   test.beforeEach(async ({ page }) => {
-    registerPage = new RegisterPage(page);
+    registerPageAccountDetails = new RegisterPage_AccountDetails(page);
+    registerPagePersonalDetails = new RegisterPage_PersonalDetails(page);
+    registerPageAddress = new RegisterPage_Address(page);
+    registerPageBottomContent = new RegisterPage_BottomContent(page);
+
     await page.goto("/#/register", { waitUntil: "networkidle" });
   });
 
-  test("Register - Provide Account Details", async ({ page }) => {
-    await registerPage.provideAccountDetailsInRegisterForm(
-      "aaaaaa",
-      "aaa@aa.pl",
-      "111Aa",
-      "111Aa"
+  test("Register - Correct Account Details", async ({ page }) => {
+    const password =
+      faker.string.alpha({ casing: "upper" }) +
+      faker.string.alpha({ casing: "lower" }) +
+      faker.string.numeric(2);
+
+    await registerPageAccountDetails.provideAccountDetailsInRegisterForm(
+      faker.internet.username().slice(1, 15),
+      faker.internet.email(),
+      password,
+      password
     );
-    await registerPage.agreeConditionsAndRegister();
+    await registerPageBottomContent.agreeConditionsAndRegister(true, true);
+  });
+
+  test("Register - Provide All Informations", async ({ page }) => {
+    const password =
+      faker.string.alpha({ casing: "upper" }) +
+      faker.string.alpha({ casing: "lower" }) +
+      faker.string.numeric(2);
+
+    await registerPageAccountDetails.provideAccountDetailsInRegisterForm(
+      faker.internet.username().slice(1, 15),
+      faker.internet.email(),
+      password,
+      password
+    );
+    await registerPagePersonalDetails.providePersonalDetailsInRegisterForm(
+      faker.person.firstName(),
+      faker.person.lastName(),
+      faker.string.numeric(9)
+    );
+    await registerPageAddress.provideAddressInRegisterForm(
+      "Poland",
+      faker.location.city(),
+      faker.location.streetAddress(),
+      faker.location.state(),
+      faker.location.zipCode()
+    );
+    await registerPageBottomContent.agreeConditionsAndRegister(true, true);
   });
 });
